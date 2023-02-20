a806ea8cffdf15820f7ea6a7200c7848e92eeb30
Aleksey Korolev <aleksey.korolev@nuance.com>
2023-02-20 03:10:00 +0300

    replaced poetry with pip

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 #poetry install
 pip install -r requirements.txt
 
-python SignupDirector/manage.py collectstatic --no-input
-python SignupDirector/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
